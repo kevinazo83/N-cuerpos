95ba18c0bedec4a64b1785f2db123bf251fbbcf8
jaabrilm <79558952+jaabrilm@users.noreply.github.com>
2022-06-27 17:43:54 -0500

    comentarios lp.cpp

--- lp.cpp
+++ lp.cpp
@@ -7,13 +7,12 @@
 #include<stdlib.h>
 //#include<conio.h>
 
-
 // Unidades:
 // Masa = Masa terrestre, Distancia = U.astronómica, tiempo = mes.
 
 //Variables globales
 double N = 20000; // Numero de pasos
-//double dt = 0.01; // Tres Dias (Mercurio no presenta problemas)
+//double dt = 0.01; // 7.7 horas (Mercurio no presenta problemas)
 // Si tomamos dt = 0.5 (Medio mes), mercurio no matiene su orbita.
 // Si tomamos dt = 0.15 (Cuatro dias y medio), la orbita de mercurio choca con la de venus.
 
@@ -21,9 +20,9 @@ double N = 20000; // Numero de pasos
 class cuerpo {
 
 	public:
-		double m;
-        double dt;
-		std::vector<double> x{0,0,0}, v{0,0,0},F{0,0,0}, A{0,0,0};  //se agregaron los vectores fuerza y aceleracion,
+		double m;                                                      //masa de cada cuerpo
+        double dt;                                                    //dt con el que se calculara la orbita de cada cuerpo
+		std::vector<double> x{0,0,0}, v{0,0,0},F{0,0,0}, A{0,0,0};   // vectores posicion, velocidad,  fuerza y aceleracion,
 
 		//Inicializar el cuerpo
 		void init(double m0, double x0, double y0, double z0, double vx0, double vy0, double vz0,double Fx0, double Fy0, double Fz0 ,double Ax0, double Ay0, double Az0,double dt0);
@@ -70,61 +69,56 @@ int main(){
 
 	//En este vector almacenamos todos los cuerpos del sistema
 	std::vector<cuerpo>Sistema;
-    std::vector<float>vNotas;
+    //vector donde se almacenan todos los datos de entrada en un vector de una fila
+    std::vector<float>datosin;
 
+    float cal = 0;        //se utiliza par extraer las componentes de los dats de entrada
 
-	//Cuerpos del sistema solar
+    std::ifstream fentrada ("entrada.txt");   //Apertura del archivo en modo lectura
 
-    float cal = 0;
-
-    std::ifstream fNotas ("entrada.txt"); //Apertura del archivo en modo lectura
-    if(fNotas.is_open()){
-
-
-         while (fNotas >> cal){
-                vNotas.push_back(cal);
+    //pasando los datos de fentrada(str) a datosin(float)
+    if(fentrada.is_open()){
+         while (fentrada >> cal){
+                datosin.push_back(cal);
          }
-    float l=vNotas[vNotas.size()-1];
+    //l es el numero de cuerpos
+         float l=datosin[datosin.size()-1];
+
+    //el vector ordenando tendra en las filas los planetas y en las columnas los datos del planeta
     std::vector<std::vector<float>>ordenando(l);
-    std::vector<std::vector<cuerpo>>ordenando1(l);
+    // pasando los datos a ordenando
     for (int k = 0; k < l; k++){
         for (int m=0; m < 14;m++){
-            ordenando[k].push_back(vNotas[(k*14)+m]);
+            ordenando[k].push_back(datosin[(k*14)+m]);
 
         }
     }
+    //inicializando los datos
     cuerpo C;
     for (int k = 0; k < l; k++){
-           //cuerpo ordenando1[k].init=ordenando[k];
-        //for (int m=0; m < 13;m++){
             C.init(ordenando[k][0],ordenando[k][1],ordenando[k][2],ordenando[k][3],ordenando[k][4],ordenando[k][5],ordenando[k][6],ordenando[k][7],ordenando[k][8],ordenando[k][9],ordenando[k][10],ordenando[k][11],ordenando[k][12],ordenando[k][13]);
             Sistema.push_back(C);
         }
-        //std::cout <<"\n";
-        //std::cout<<std::endl;
+
     }
-    //std::cout<<std::endl;
+    //cerrando el archivo de entrada
+    fentrada.close();
 
-    
-    fNotas.close();
-
-
-
-
+    //se abre o se crea el archivo datos.txt en modoescritura, donde saldran las soluciones calculadas
     std::ofstream outfile;
     outfile.open("datos.txt");
 	//Ciclo para N-1 pasos de tiempo (imprime N valores para cada cuerpo)
 	for (int i = 0; i < N; i++){
 
-         for (int j = 0; j < Sistema.size(); j++){
+        for (int j = 0; j < Sistema.size(); j++){
+            //da el siguiente paso y se imprime en datos.txt, el algoritmo se autoinicia
              Tstep(Sistema.at(j));
-             //Sistema.at(j).print();
 			 outfile<< Sistema.at(j).x.at(0) << "\t\t" << Sistema.at(j).x.at(1) << "\t\t" << Sistema.at(j).x.at(2)<<"\n";
 
-         }
+        }
+        //limpia todas las fuerzas sobre un cuerpo, para dar e siguiente paso
          for (int j = 0; j < Sistema.size(); j++){
              limpF(Sistema.at(j));
-             //Sistema.at(j).print();
          }
 		//Interacion entre todos los cuerpos del sistema
 		 for (int j = 0; j < Sistema.size(); j++){
@@ -135,23 +129,25 @@ int main(){
 			}
 		}
 
-		//Imprime y actualiza los cuerpos
+		//actualiza velocidad y aceleracion
 		 for (int j = 0; j < Sistema.size(); j++){
              Vstep(Sistema.at(j));
 		 }
+         //termina deescribir sobre datos.txt
 		 outfile<<std::endl;
 	}
+    //cierra datos.txt
     outfile.close();
 	return 0;
 }
-void limpF(cuerpo&a){  //funcion para hacer la fuerza total sobre un cuerpo igual a cero, al iniciar una nueva iteracion
+void limpF(cuerpo&a){  //funcion para hacer la fuerza total sobre un cuerpo igual a cero, al iniciar una nueva interacion
 
 	a.F.at(0) =0 ;
 	a.F.at(1) =0;
 	a.F.at(2) =0;
 }
 
-void NewF(cuerpo &a, cuerpo &b){       //calcula la fuerza que ejerce el cuerpo b sobre el cuerpo a, Jack calculaba la aceleración, lo cambie para poder implementar mejor lp
+void NewF(cuerpo &a, cuerpo &b){       //calcula la fuerza que ejerce el cuerpo b sobre el cuerpo a,
 
 	std::vector<double> R{0,0,0};
 	double r = 0;
@@ -166,12 +162,10 @@ void NewF(cuerpo &a, cuerpo &b){       //calcula la fuerza que ejerce el cuerpo 
 
 	double r3 = pow(r,3);
 	double G = 80.79*pow(10,-8);
-    //double G = 1200;
-	// Aceleraciones de los cuerpos
-	// F = -Gm1m2/|r|^3 * r y F = ma
-	double Fa = -G*b.m*a.m/r3;
-	//double Fb = -G*a.m*b.m/r3;
 
+	// fuerza sobre lel cuerpo a
+
+	double Fa = -G*b.m*a.m/r3;
 
 	a.F.at(0) += Fa*R.at(0)*1;
 	a.F.at(1) += Fa*R.at(1)*1;
